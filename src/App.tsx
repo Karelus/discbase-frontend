75c468640e6972581f3282d63b19a11ad4fb5968
Karel Haapasalo <karel.haapasalo@hotmail.com>
2022-04-08 18:50:06 +0300

    Add test for mockend

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from "react";
-import "./App.css";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    fetch("https://mockend.com/Karelus/discbase-frontend/posts")
+      .then((response) => console.log(response))
+      .catch((e) => console.error(e))
+      .finally(() => console.log("something went wrong"));
+  }, []);
+
   return (
     <div className="App">
       <h2>Foo</h2>
