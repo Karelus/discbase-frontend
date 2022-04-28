1336b4c9b6e49f9d0572e25602f11505c178cb7e
Karel Haapasalo <karel.haapasalo@hotmail.com>
2022-04-28 10:49:17 +0300

    Remove welcome text from Main Container

--- src/Components/MainContainer.tsx
+++ src/Components/MainContainer.tsx
@@ -10,29 +10,6 @@ const MainContainer: FC = ({ children }) => {
       id="main-container"
       sx={{ margin: 4, background: theme.palette.background.default }}
     >
-      <Box
-        id="welcome-text-container"
-        sx={{
-          display: "flex",
-          flexWrap: "wrap",
-          mb: 2,
-        }}
-      >
-        <Typography
-          sx={{ width: "100%", mb: 2, fontWeight: 500 }}
-          textAlign="center"
-          variant="h3"
-        >
-          {constants.welcoleText}
-        </Typography>
-        <Typography
-          sx={{ width: "100%", fontWeight: 500 }}
-          textAlign="center"
-          variant="h4"
-        >
-          {constants.welcomeSubText}
-        </Typography>
-      </Box>
       {children}
     </Box>
   );
